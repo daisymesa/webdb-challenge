86bc6b78636907f79cd08134c8dc90caf40e891d
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-06-21 18:42:53 -0400

    Setting up CRUD for projects

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -1,62 +1,65 @@
 const express = require('express');
-
 const router = require('express').Router();
 
 const knex = require('knex');
-
 const knexConfig = require('../knexfile.js');
-
-/*
-const knexConfig = {
-    client: 'sqlite3',
-    connection: {
-        filename: './data/lambda.db3'
-    },
-    useNullAsDefault: true
-}
-*/
-
 const db = knex(knexConfig.development);
 
-/*
-router.get('/', (req, res) => {
-    res.send(`
-    <h2>Projects is working!</h2>
-    `)
-})
-*/
 
-// CREATE
+// CREATE - add a project
 router.post('/', (req, res) => {
+    const newProject = { name: req.body.name, description: req.body.description };
 
+    db('projects')
+        .insert(newProject)
+        .then(addedProject => {
+            res.status(201).json(addedProject);
+        })
+        .catch(error => {
+            res.status(500).json({ error: 'The new project could not be added.' })
+        })
 })
 
-// READ
+// READ - get all projects
 router.get('/', (req, res) => {
     db('projects')
-    .then(projects => {
+        .then(projects => {
+            res.status(200).json(projects);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        })
+})
+
+// READ - get a project by id
+router.get('/:id', (req, res) => {
+    db('projects')
+      .where({ id: req.params.id })
+      .first()
+      .then(projects => {
         res.status(200).json(projects);
     })
     .catch(error => {
         res.status(500).json(error);
     })
-})
-
-// READ - get by id
-router.get('/:id', (req, res) => {
-
-})
-
-// UPDATE
-router.put('/', (req, res) => {
-
-})
-
+  });
 
 // DELETE
-router.delete('/', (req, res) => {
-
-})
-
+router.delete('/:id', (req, res) => {
+    db('projects')
+        .where({ id: req.params.id })
+        .del()
+        .then(count => {
+            if (count > 0) {
+                res.status(204).end();
+            } else {
+                res.status(404).json({ message: 'Project not found' });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
 
 module.exports = router;
+
